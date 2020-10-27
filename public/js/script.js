8306c6f47078d65b2e8a0c9e749e5821b7bc1594
Mikael FLORA <5983947+mikaelflora@users.noreply.github.com>
2020-10-27 09:29:26 +0100

    skill

--- public/js/script.js
+++ public/js/script.js
@@ -1,9 +1,9 @@
 $(window).scroll(function() {
 	if ($(document).scrollTop() > 64) {
 		$('nav').removeClass('bg-transparent');
-		$('nav').addClass('bg-dark');
+		$('nav').addClass('bg-primary');
 	} else {
-		$('nav').removeClass('bg-dark');
+		$('nav').removeClass('bg-primary');
 		$('nav').addClass('bg-transparent');
 	}
 });
