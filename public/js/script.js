79bc26e5d6517ed65c00b7ea5bc985383fa5a7be
Mikael FLORA <5983947+mikaelflora@users.noreply.github.com>
2020-11-27 08:54:05 +0100

    ajout des animations pour l'apparition des compétences

--- public/js/script.js
+++ public/js/script.js
@@ -7,3 +7,15 @@ $(window).scroll(function() {
 		$('nav').addClass('bg-transparent');
 	}
 });
+
+$(window).scroll(function () {
+	const topSkill = $('#skill').offset().top;
+	const bottomSkill = $('#skill').offset().top + $('#skill').outerHeight();
+	const topScreen = $(window).scrollTop();
+	const bottomScreen = $(window).scrollTop() + $(window).innerHeight();
+	if ((bottomScreen > topSkill) && (topScreen < bottomSkill)) {
+		$('.left').addClass('fade-in-left');
+		$('.down').addClass('fade-in-down');
+		$('.right').addClass('fade-in-right');
+	}
+})
